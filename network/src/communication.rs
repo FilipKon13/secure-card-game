518cab7ed4a464ba0565089a80040a0e016b8a03
Rafal Pyzik <rafal.pyzik@student.uj.edu.pl>
2024-06-19 12:47:44 +0200

    improved communication

--- network/src/communication.rs
+++ network/src/communication.rs
@@ -1,7 +1,8 @@
 use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
 
-use crate::message::Message;
+use serde::de::DeserializeOwned;
+use serde::Serialize;
 
 const ADDRESS: &str = "127.0.0.1:6789";
 
@@ -34,24 +35,36 @@ impl Communication {
         }
     }
 
-    pub fn send(&mut self, message: &Message) {
+    pub fn send<T: Serialize>(&mut self, message: &T) {
         let serialized = serde_json::to_string(&message).unwrap();
         let stream = self.stream.as_mut().unwrap();
-        stream.write(serialized.as_bytes()).unwrap();
+        let length = serialized.len() as u32;
+        let lenght_bytes = length.to_le_bytes();
+        stream.write_all(&lenght_bytes).unwrap();
+        stream.write_all(serialized.as_bytes()).unwrap();
     }
 
-    pub fn receive(&mut self) -> Message {
+    fn read_u32(&mut self) -> u32 {
         let stream = self.stream.as_mut().unwrap();
-        let mut buffer = [0; 512];
-        let size = stream.read(&mut buffer).unwrap();
-        let serialized = String::from_utf8_lossy(&buffer[0..size]).to_string();
-        let deserialized: Message = serde_json::from_str(&serialized).unwrap();
+        let mut buffer = [0; 4];
+        stream.read_exact(&mut buffer).unwrap();
+        u32::from_le_bytes(buffer)
+    }
+
+    pub fn receive<T: DeserializeOwned>(&mut self) -> T {
+        let length = self.read_u32();
+        let stream = self.stream.as_mut().unwrap();
+        let mut buffer = vec![0 as u8; length as usize];
+        stream.read_exact(&mut buffer).unwrap();
+        let serialized = String::from_utf8_lossy(&buffer.as_slice()).to_string();
+        let deserialized: T = serde_json::from_str(&serialized).unwrap();
         deserialized
     }
 }
 
 #[cfg(test)]
 mod test {
+    use crate::message::Message;
     use std::{thread, time::Duration};
 
     use super::*;
@@ -63,7 +76,7 @@ mod test {
             println!("starting server");
             server.start_server();
 
-            let msg = server.receive();
+            let msg: Message = server.receive();
             println!("server received: {} {}", msg.x, msg.s);
             assert_eq!(msg.x, 5);
 
@@ -83,7 +96,7 @@ mod test {
             s: String::from("hello1"),
         });
 
-        let msg = client.receive();
+        let msg: Message = client.receive();
         println!("client received: {} {}", msg.x, msg.s);
         assert_eq!(msg.x, 7);
     }
